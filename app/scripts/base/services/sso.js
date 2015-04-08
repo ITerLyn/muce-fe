e0a20018730c455eb0aa85976eb0224aef08d2d8
moonger <moonger@wandoujia.com>
2015-04-08 14:55:12 +0800

    fix bugs : MQ

--- app/scripts/base/services/sso.js
+++ app/scripts/base/services/sso.js
@@ -1,6 +1,6 @@
 define(function() {
     var SSO_LOCATION = 'http://sso.wandoulabs.com/';
-    var whiteList = ['gaohailang', 'zuxinglong', 'gaiyongjuan', 'weijin', 'haiyating', 'songzi'];
+    var whiteList = ['gaohailang', 'zuxinglong', 'gaiyongjuan', 'weijin', 'haiyating', 'songzi', 'zhengwenjie'];
 
     angular.module('muceApp.base.services.sso', [])
         .provider('ssoInterceptor', function() {
